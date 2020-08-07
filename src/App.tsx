7849a169e4223827b9c59a5b7491f8ffe99ad7e0
sogafish <k.sogafish@gmail.com>
2020-08-08 01:37:27 +0900

    :contruction: NavigationContainer Wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { GoogleSignIn } from './pages';
+
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <GoogleSignIn />
-    </>
+    </NavigationContainer>
   );
 };
 
