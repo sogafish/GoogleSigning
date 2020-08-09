e7d76b0b886c9d31183e69073e7b499d53d460f3
sogafish <k.sogafish@gmail.com>
2020-08-09 17:56:26 +0900

    :hammer: Fix Format

--- src/pages/GoogleSignIn/index.tsx
+++ src/pages/GoogleSignIn/index.tsx
@@ -1,16 +1,17 @@
-import React, { memo, useCallBack, useEffect } from 'react';
-import { ScrollView, SafeAreaView, Text } from 'react-native';
+import React, { memo, useEffect } from 'react';
+import { ScrollView, SafeAreaView } from 'react-native';
 import {
   GoogleSignin,
   GoogleSigninButton,
 } from '@react-native-community/google-signin';
 import { styles } from './styles';
 
-export const GoogleSignIn = memo(() => {
+export const GoogleSignIn = memo(function GoogleSignIn() {
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId: '281361953262-45v058vqni3j617ra33sionbc86op114.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      webClientId:
+        '281361953262-45v058vqni3j617ra33sionbc86op114.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
       hostedDomain: '', // specifies a hosted domain restriction
       loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
@@ -18,9 +19,8 @@ export const GoogleSignIn = memo(() => {
       accountName: '', // [Android] specifies an account name on the device that should be used
       // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
     });
-  });
+  }, []);
 
-  let text = '';
   const onPress = React.useCallback(async () => {
     await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin.signIn();
@@ -35,7 +35,6 @@ export const GoogleSignIn = memo(() => {
           color={GoogleSigninButton.Color.Dark}
           onPress={onPress}
         />
-        <Text>{text}</Text>
       </ScrollView>
     </SafeAreaView>
   );
